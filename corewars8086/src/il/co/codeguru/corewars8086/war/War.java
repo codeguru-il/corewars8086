08059d46a619a54c4ccf698d218d900a36ef2fda
yovaayova <yoav.kaempfer@gmail.com>
2014-04-25 16:47:09 +0300

    The order of the warrior groups inside each war is random, including the zombies.

--- corewars8086/src/il/co/codeguru/corewars8086/war/War.java
+++ corewars8086/src/il/co/codeguru/corewars8086/war/War.java
@@ -7,6 +7,7 @@ import il.co.codeguru.corewars8086.memory.RealModeAddress;
 import il.co.codeguru.corewars8086.memory.RealModeMemoryImpl;
 import il.co.codeguru.corewars8086.utils.Unsigned;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -85,8 +86,15 @@ public class War {
      */
     public void loadWarriorGroups(WarriorGroup[] warriorGroups) throws Exception {
         m_currentWarrior = 0;
-        for (int i = 0; i < warriorGroups.length; ++i) {
-            loadWarriorGroup(warriorGroups[i]);
+        ArrayList<WarriorGroup> groupsLeftToLoad = new ArrayList<WarriorGroup>();
+        for (int i = 0; i < warriorGroups.length; ++i)
+        	groupsLeftToLoad.add(warriorGroups[i]);
+               
+        while (groupsLeftToLoad.size() > 0)
+        {
+        	int randomInt = rand.nextInt(groupsLeftToLoad.size());
+        	loadWarriorGroup(groupsLeftToLoad.get(randomInt));
+        	groupsLeftToLoad.remove(randomInt);
         }
     }
 	
