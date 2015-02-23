5b118435ae8ee0de959c96e2edf00db400e9fec1
Yovaayova <yoav.kaempfer@gmail.com>
2015-02-23 19:19:42 +0200

    Files got lost in the last commit

---
+++ src/main/java/il/co/codeguru/corewars8086/memory/MemoryException.java
@@ -0,0 +1,15 @@
+package il.co.codeguru.corewars8086.memory;
+
+/**
+ * Base class for all Exceptions thrown by the RealModeMemory classes.
+ * 
+ * @author DL
+ */
+public class MemoryException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public MemoryException(String message)
+	{
+		super(message);
+	}
+}
