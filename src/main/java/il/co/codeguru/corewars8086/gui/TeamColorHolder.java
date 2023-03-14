6e005eea5e5278eee89f6c362d8aa6d2913cb5db
Rotem Moses <rootatkali4@gmail.com>
2023-03-14 23:21:53 +0200

    CR Fixes

--- src/main/java/il/co/codeguru/corewars8086/gui/TeamColorHolder.java
+++ src/main/java/il/co/codeguru/corewars8086/gui/TeamColorHolder.java
@@ -17,25 +17,33 @@ public class TeamColorHolder {
     private final Hashtable<String, Color> colorHolder;
     private final int teamInitialsLength;
 
+    private static final int COLUMNS_IN_COLORS_FILE = 2;
+
     private String getTeamInitials(String team) {
         return team.substring(0, Math.min(this.teamInitialsLength, team.length())).toUpperCase();
     }
 
     public TeamColorHolder(String[] teamNames, int teamInitialsLength, Options options) {
-        // see http://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
         colorHolder = new Hashtable<>();
         this.teamInitialsLength = teamInitialsLength;
 
         fillCenterColors(options.colorsFile);
+        fillTeamColors(teamNames);
+    }
 
-        float golden_ratio_conjugate = 0.618033988749895f;
+    private void fillTeamColors(String[] teamNames) {
+        // see http://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
+
+        final float SATURATION = 0.8f;
+        final float BRIGHTNESS = 0.95f;
+        final float GOLDEN_RATIO_CONJUGATE = 0.618033988749895f;
+
         float x = 0;
         for (String teamName : teamNames) {
             String teamInitials = getTeamInitials(teamName);
             if (!colorHolder.contains(teamInitials)) {
-                colorHolder.put(teamInitials, Color.getHSBColor(x % 1, 0.8f, 0.95f));
-                x += golden_ratio_conjugate;
-
+                colorHolder.put(teamInitials, Color.getHSBColor(x % 1, SATURATION, BRIGHTNESS));
+                x += GOLDEN_RATIO_CONJUGATE;
             }
         }
     }
@@ -48,9 +56,9 @@ public class TeamColorHolder {
             List<String> lines = Files.readAllLines(colorsFile.toPath());
 
             for (String line : lines) {
-                String[] split = line.split(",\\s*", 2);
+                String[] split = line.split(",\\s*", COLUMNS_IN_COLORS_FILE);
 
-                if (split.length < 2) continue;
+                if (split.length < COLUMNS_IN_COLORS_FILE) continue;
 
                 String center = split[0];
                 String rawColor = split[1];
@@ -67,21 +75,19 @@ public class TeamColorHolder {
     /**
      * Decodes a hex string of the format {@code #RRGGBB} (e.g., {@code "#00FF00"}) to a {@link java.awt.Color}
      */
-    private Color decodeHexColor(String color) {
-        if (!color.matches("#[0-9a-fA-F]{6}")) {
-            throw new NumberFormatException(String.format("%s is not a valid color string", color));
+    private Color decodeHexColor(String rawColor) {
+        if (!rawColor.matches("#[0-9a-fA-F]{6}")) {
+            throw new NumberFormatException(String.format("%s is not a valid color string", rawColor));
         }
 
-        String hstr = color.replace("#", "0x");
-        return Color.decode(hstr);
+        String hexColor = rawColor.replace("#", "0x");
+        return Color.decode(hexColor);
     }
 
     public Color getColor(String team, boolean darker) {
         String teamInitials = getTeamInitials(team);
-        if (darker) {
-            return colorHolder.get(teamInitials).darker();
-        } else {
-            return colorHolder.get(teamInitials);
-        }
+        Color color = colorHolder.get(teamInitials);
+
+        return darker ? color.darker() : color;
     }
 }
